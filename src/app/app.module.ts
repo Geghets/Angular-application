53ff8cac7cd6c2ae8bc5cf1226466f327c2447c7
Geghets <geghecik.balayan@gmail.com>
2020-07-27 10:57:01 +0400

    Inline and External templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {ServerComponent} from './Server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
+  exports: [ServersComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
