dbaaf43e34bffd809e25195dd703ed56c3e476b0
Geghets <geghecik.balayan@gmail.com>
2020-07-28 16:25:02 +0400

    Forms of Databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   //selector: '[app-servers]',
   //selector: '.app-servers',
   selector: 'app-servers',
-  template: `      
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
+  /*  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+    `,*/
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
+  serverName = 'TestServer';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement> event.target).value;
+  }
 }
